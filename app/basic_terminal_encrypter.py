627754a4e3e802483e321c3bcdfb29b1ad1324bc
aad-1083474 <1083474@hr.nl>
2024-10-07 19:43:52 +0200

    FEAT: Encrypts / decrypts user input

--- app/basic_terminal_encrypter.py
+++ app/basic_terminal_encrypter.py
@@ -2,11 +2,17 @@ from cryptography.fernet import Fernet
 
 key = Fernet.generate_key()
 
-encrypted_message = input("Geef de tekst door die u wilt versleutelen:")
+encrypted_message = input("Hoi Diederik! Geef de tekst door die u wilt versleutelen:")
 
 engine = Fernet(key)
 
 # Omzetten naar bytes
 encoded_message = encrypted_message.encode()
 
-print(engine.encrypt(encoded_message))
+# Geëncrypte uitkomst
+encrypted_output = engine.encrypt(encoded_message)
+print(f"Versleutelde boodschap: {encrypted_output}")
+
+#Gedecrypte uitkomst
+decrypted_output = engine.decrypt(encrypted_output)
+print(f"Ontsleutelde boodschap: {decrypted_output.decode()}")
