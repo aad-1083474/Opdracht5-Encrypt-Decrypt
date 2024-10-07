98fd11ba0983f0ff03a8f4109e54029446f3af51
aad-1083474 <1083474@hr.nl>
2024-10-07 19:37:42 +0200

    FEAT: start encryption app

--- app/basic_terminal_encrypter.py
+++ app/basic_terminal_encrypter.py
@@ -1,1 +1,11 @@
-import random
+from cryptography.fernet import Fernet
+
+key = Fernet.generate_key()
+
+encrypted_message = input("Geef de tekst door die u wilt versleutelen:")
+
+engine = Fernet(key)
+
+
+
+print(engine.encrypt(encrypted_message))
